079c26d34f6c756e1f7d380d4b1339a73362fc5c
Justyn Durnford <jpdurnford@alaska.edu>
2021-05-24 16:14:04 -0800

    -Added include\JLibrary\System\Clock.hpp -Added src\JLibrary\System\Clock.cpp

---
+++ JLibraryDevelopment/JLibrary-1.0.0/include/JLibrary/System/Clock.hpp
@@ -0,0 +1,45 @@
+// JLibraryDevelopment
+// Clock.hpp
+// Created on 2021-05-24 by Justyn Durnford
+// Last modified on 2021-05-24 by Justyn Durnford
+// Header file for the Clock class.
+
+#pragma once
+
+#include <chrono>
+
+namespace jl
+{
+	// 
+	class Clock
+	{
+		std::chrono::time_point<std::chrono::system_clock> start_;
+
+		public:
+
+		// Default constructor.
+		//
+		Clock();
+
+		// Copy constructor.
+		Clock(const Clock& other) = delete;
+
+		// Move constructor.
+		Clock(Clock&& other) = delete;
+
+		// Copy assignment operator.
+		Clock& operator = (const Clock& other) = delete;
+
+		// Move assignment operator.
+		Clock& operator = (Clock&& other) = delete;
+
+		// Destructor.
+		~Clock() = default;
+
+		// 
+		std::chrono::duration<double> timeElapsed() const;
+
+		// 
+		std::chrono::duration<double> restart();
+	};
+}
---
+++ JLibraryDevelopment/JLibrary-1.0.0/src/JLibrary/System/Clock.cpp
@@ -0,0 +1,27 @@
+// JLibraryDevelopment
+// Clock.cpp
+// Created on 2021-05-24 by Justyn Durnford
+// Last modified on 2021-05-24 by Justyn Durnford
+// Source file for the Clock class.
+
+#include <JLibrary/System/Clock.hpp>
+using namespace std;
+using namespace chrono;
+using namespace jl;
+
+Clock::Clock()
+{
+	start_ = system_clock::now();
+}
+
+duration<double> Clock::timeElapsed() const
+{
+	return system_clock::now() - start_;
+}
+
+duration<double> Clock::restart()
+{
+	duration<double> time_elapsed(system_clock::now() - start_);
+	start_ = system_clock::now();
+	return time_elapsed;
+}
