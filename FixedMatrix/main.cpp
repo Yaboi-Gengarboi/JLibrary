a303adf5f787b0d2779e49c7fb48034df05331de
Justyn Durnford <jpdurnford@alaska.edu>
2022-01-17 18:15:33 -0900

    Created a stand-alone FixedMatrix project.

---
+++ FixedMatrix/main.cpp
@@ -0,0 +1,12 @@
+// FixedMatrix
+// main.cpp
+// Created on 2022-01-15 by Justyn Durnford
+// Last modified on 2022-01-15 by Justyn Durnford
+// Test file for the FixedMatrix template class.
+
+import FixedMatrix;
+
+int main()
+{
+	return 0;
+}
