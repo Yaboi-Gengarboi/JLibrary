b60cb1ebe6ce6f43c77a9d85eec72fdff9e5368a
Justyn Durnford <jpdurnford@alaska.edu>
2022-02-17 14:28:36 -0900

    -Updated JLibrary.hpp to reflect the recent changes to the JLibrary. -Cleaned up main.cpp.

--- JLibrary/JLibrary.hpp
+++ JLibrary/JLibrary.hpp
@@ -1,13 +1,13 @@
 // JLibrary
 // JLibrary.hpp
 // Created on 2021-08-06 by Justyn Durnford
-// Last modified on 2022-01-09 by Justyn Durnford
+// Last modified on 2022-02-13 by Justyn Durnford
 // Header file that includes all of the JLibrary.
 
 #pragma once
 
 #include "Angle.hpp"
-#include "Clock.hpp"
+#include "Arithmetic.hpp"
 #include "Color.hpp"
 #include "Constants.hpp"
 #include "Direction.hpp"
@@ -15,21 +15,29 @@
 #include "Hexadecimal.hpp"
 #include "IntegerTypedefs.hpp"
 #include "MiscStringFunctions.hpp"
+#include "Time.hpp"
 
 import Array;
-import Arithmetic;
 import Circle;
+import ComplexNumber;
+import Equation;
 import FixedArray;
 import FixedMatrix;
 import Fraction;
+import IDClass;
+import LinearEquation1;
+import LinearEquation3;
 import LineSegment;
 import Matrix;
 import MiscTemplateFunctions;
+import Plane;
+import Polynomial;
 import Ptr;
 import Rect;
-import SFML_JLIB_Convert;
+import SFML_JLIB;
 import Square;
 import Triangle;
 import Vector2;
 import Vector3;
+import VectorEquation3;
 import VectorN;
--- JLibrary/main.cpp
+++ JLibrary/main.cpp
@@ -1,7 +1,7 @@
 // JLibrary
 // main.cpp
 // Created on 2022-01-08 by Justyn Durnford
-// Last modified on 2022-01-15 by Justyn Durnford
+// Last modified on 2022-02-16 by Justyn Durnford
 // Test file for the JLibrary static library.
 
 #include "JLibrary.hpp"
@@ -9,52 +9,14 @@ using namespace jlib;
 
 #include <iostream>
 using std::cout;
-using std::endl;
-
-#include <string>
-using std::string;
-using std::size_t;
-using std::to_string;
-
-template <typename T, size_t N>
-void print_array(const FixedArray<T, N>& arr)
-{
-	string str("{ ");
-	const size_t len = N - 1;
-
-	for (size_t i = 0; i < len; ++i)
-		str += to_string(arr[i]) + ", ";
-
-	str += to_string(arr[len]) + " }";
-
-	cout << str << endl;
-}
-
-template <typename T>
-void print_array(const Array<T>& arr)
-{
-	string str("{ ");
-	const size_t len = arr.size() - 1;
-
-	for (size_t i = 0; i < len; ++i)
-		str += to_string(arr[i]) + ", ";
-
-	str += to_string(arr[len]) + " }";
-
-	cout << str << endl;
-}
 
 int main()
 {
-	FixedMatrix<i16, 3, 3> mat3x3
-	{
-		{0, 1, 2},
-		{3, 4, 5},
-		{6, 7, 8}
-	};
+	Vector4i A = { 1, 1, 1, 1 };
+	Vector4f B(A.unitVector());
 
-	FixedArray<i16, 3> row0(mat3x3.getRow(0));
-	print_array(row0);
+	cout << B << '\n';
+	cout << B.magnitude() << '\n';
 
 	return 0;
 }
